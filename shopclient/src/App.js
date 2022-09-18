1b96b8030867a1e78d33a9d878b61b25af0b9cfc
satyam <bhasinsatyam7@gmail.com>
2022-09-18 23:55:12 +0530

    first commit

---
+++ shopclient/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href='/'>Satyam-Zone</a>
+      </header>
+      <main>List of Products</main>
+    </div>
+  )
+}
+
+export default App
